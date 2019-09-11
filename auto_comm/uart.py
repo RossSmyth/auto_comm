88376d3a4362fca9f6ba952c95269965052b84c8
Ross Smyth <crsmyth@mtu.edu>
2019-09-11 10:49:24 -0400

    Initial read override method.

--- auto_comm/uart.py
+++ auto_comm/uart.py
@@ -21,3 +21,18 @@ class UART(serial.Serial):
         assert 'UART' in ini.sections()
                 
         super().__init__(**get_uart_options(ini))
+        
+    def read(self, size: int = 1, hexa: bool = True):
+        """Override of the serial.Serial superclass's read() method
+        Returns an array of the read data seperated by byte.
+        
+        Positional Arguments:
+        ---------------------
+        size = The number of bytes to read - int (default = 1)
+        hexa = Whether to spit out hex or not, if not it will not touch it.
+        """
+        out = super.read(size)
+        
+        log(out) # TODO
+        
+        return hex(out) if hexa else hex
